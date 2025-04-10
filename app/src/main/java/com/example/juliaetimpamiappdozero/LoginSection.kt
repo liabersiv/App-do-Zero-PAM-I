dc44f47bbc3c2e68a842f17d5c172d7b82b07a3d
liabersiv <juliaribeiro.dsv@gmail.com>
2025-04-09 22:16:32 -0300

    fazendo a tela incial e tela de login

---
+++ app/src/main/java/com/example/juliaetimpamiappdozero/LoginSection.kt
@@ -0,0 +1,11 @@
+package com.example.juliaetimpamiappdozero;
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+public class LoginSection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginsection)
+    }
+}
