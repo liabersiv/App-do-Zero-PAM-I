254fb436ab6e9eb7d9e58a245a30a92f981aa2e5
liabersiv <juliaribeiro.dsv@gmail.com>
2025-04-09 23:00:35 -0300

    initial page

---
+++ app/src/main/java/com/example/juliaetimpamiappdozero/InitialPage.kt
@@ -0,0 +1,11 @@
+package com.example.juliaetimpamiappdozero
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InitialPage : AppCompatActivity{
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initialpage)
+    }
+}
